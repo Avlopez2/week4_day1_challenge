3f31944a0f96778a1ae6597ab0bdfb5dba3faef0
Lopez <avlopez2@cps.k12.il.us>
2024-09-19 12:12:52 -0500

    week4_day1_challenge

--- week4_activity.py
+++ week4_activity.py
@@ -4,26 +4,49 @@
 ##########################Reviewing somethings
 
 # indexing strings -- slide 4
-my_text = 'this is a text'
+my_text = 'this is a text string ' # keep a space at the end of the text you need to keep a space just incase you want to print out the whole letter
 result = my_text  #get the index of the letter a
-print(result)
+print(result[0:4])
+#find the word text
+print (result.find('text'))
+#find the index of the word text 
+print (result[10:14])
+#hwo do I find the word "is"
+print(result.find('is'))
+print(result[5:7])
+print(result.find('string'))
+print(result[15:-1])
+#print(word.find[8:14])# doesn't work because variable word.find doesnt exists
+#print(word[8:6]) #variable word doesnt exisit so it doesnt work.
+print(result.find('hancock'))# when -1 appears it means the substring does no exsits 
+#reverse the string
+#print(result[::-1])
+#get the last letter of the entire string
+print(result[-1])
+#index slicing is when you get  a substring of a string. 
+#specifically the start and end of an index of the substring. 
+#The start index is inclusive and the end is exclusive. 
 
 #get the index of the third letter from the end of the text
-
+print(result[-3])
+print(result[-5])
 # find the index of the letter s
-
+print(result[6])
 ###slide 5
 # string[start:stop:step]
+
 #example
-# text = "Hello, World!"
-# print(text[7:12])  # prints "World"
+text = "Hello, World!"
+print(text[7:12])  # prints "World"
 text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # Get the substring CDE and put in a variable
-
+substring=text.find('CDE')
+print(substring)
+substring = text[2:5]
 #get the first letter all the way to the 4th letter
-
+print(text[0:4])
 #get the first letter to the final letter and skip every 3rd letter
-
+print(text[0::3])
 # Built-in methods:
 # Python has a variety of built-in methods to work with substrings.
 
